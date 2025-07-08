6649864abecacad639843352935002312abd16df
Simon Blum <simon.blum@airbus.com>
2025-07-08 15:07:24 +0200

    fix: update logging

--- main/main.c
+++ main/main.c
@@ -1,4 +1,5 @@
 #include "esp_event.h"
+#include "esp_http_client.h"
 #include "esp_http_server.h"
 #include "esp_log.h"
 #include "esp_netif_types.h"
@@ -21,23 +22,28 @@
 
 static const char *TAG = "app_main";
 
+#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
 static void on_wifi_connect(void *arg, esp_event_base_t event_base,
                             int32_t event_id, void *event_data) {
-  httpd_handle_t *server = (httpd_handle_t *)arg;
-  if (*server == NULL) {
-    ESP_LOGI(TAG, "Wi-Fi connected, starting HTTP server");
-    *server = server_start();
-  }
+  ESP_LOGI(TAG, "Wi-Fi connected");
+
+  // httpd_handle_t *server = (httpd_handle_t *)arg;
+  // if (*server == NULL) {
+  //   *server = server_start();
+  // }
 }
 
 static void on_wifi_disconnect(void *arg, esp_event_base_t event_base,
                                int32_t event_id, void *event_data) {
-  httpd_handle_t *server = arg;
-  if (*server) {
-    ESP_LOGI(TAG, "Wi-Fi lost, stopping HTTP server");
-    server_stop(*server);
-    *server = NULL;
-  }
+  ESP_LOGI(TAG, "Wi-Fi lost");
+
+  // httpd_handle_t *server = arg;
+  // if (*server) {
+  //   server_stop(*server);
+  //   *server = NULL;
+  // }
 }
 
 void app_main(void) {
@@ -47,7 +53,7 @@ void app_main(void) {
   ESP_ERROR_CHECK(wifi_init_sta());
   ESP_ERROR_CHECK(sensor_init(GPIO_NUM_4));
 
-  static httpd_handle_t server = NULL;
+  // static httpd_handle_t server = NULL;
 
   /* register our start/stop callbacks */
   ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
@@ -55,11 +61,24 @@ void app_main(void) {
   ESP_ERROR_CHECK(esp_event_handler_register(
       WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect, &server));
 
-    SessionResult session_result;
-    sensor_measure_session(&session_result);
-  server = server_start();
+  // server = server_start();
 
-  while (server) {
-    sleep(5);
+  SessionResult session_result;
+  while (true) {
+    ESP_LOGI(TAG, "Measuring session...");
+    sensor_measure_session(&session_result);
+    ESP_LOGI(TAG, "Done measuring session, taking image...");
+
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) {
+      ESP_LOGE(TAG, "Camera capture failed");
+    } else {
+      ESP_LOGI(TAG, "Image taken")
+    }
+
+    ESP_LOGI("Would now send http request...");
+
+    ESP_LOGI(TAG, "Sleeping 1 second...");
+    sleep(1);
   }
 }
